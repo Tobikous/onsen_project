4bfbb29b898f24ba12f1adcd964d3ee704bf585f
Tobikous <tobinanonano1996@gmail.com>
2023-02-24 22:45:08 +0900

    breeze install

--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
+});
 
-
-Auth::routes();
-// Route::get('/login/guest', [App\Http\Controllers\Auth\LoginController::class, 'guestLogin'])->name('/login/guest');
-
-
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/create', [App\Http\Controllers\ArticleController::class, 'create'])->name('create');
-Route::post('/store', [App\Http\Controllers\ArticleController::class, 'store'])->name('store');
-Route::get('/show/{id}', [App\Http\Controllers\ArticleController::class, 'show'])->name('show');
-Route::get('/article', [App\Http\Controllers\ArticleController::class, 'article'])->name('article');
-Route::get('/edit/{id}', [App\Http\Controllers\ArticleController::class, 'edit'])->name('edit');
-Route::post('/update/{id}', [App\Http\Controllers\ArticleController::class, 'update'])->name('update');
-Route::post('/delete/{id}', [App\Http\Controllers\ArticleController::class, 'delete'])->name('delete');
-Route::post('/upload', [App\Http\Controllers\ArticleController::class, 'upload'])->name('upload');
+require __DIR__.'/auth.php';
