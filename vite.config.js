c5441ffdc81d93bb0b781a07a4a089827712db85
Tobikous <tobinanonano1996@gmail.com>
2023-03-01 20:01:45 +0900

    vite fixed

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    plugins: [
-        laravel([
-            'resources/css/app.css',
-            'resources/js/app.js',
-        ]),
-    ],
-});
+  plugins: [
+    laravel([
+      'resources/sass/app.scss',
+      'resources/js/app.js',
+      'resources/css/app.css', 
+    ]),
+    vue(),
+  ],
+  server: {
+    host: true,
+    hmr: {
+      host: 'localhost',
+  },
+  },
+})
